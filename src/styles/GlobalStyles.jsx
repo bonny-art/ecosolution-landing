7e6ac81bcbf844ed2d52320ae77c74eacbbd8f89
bonny-art <svitlana.otenko@gmail.com>
2024-07-06 14:27:55 +0300

    Setup basic project settings
    Setup basic project settings: fonts, theme colors, favicon, site description, and logo file

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,102 @@
+import { css } from '@emotion/react';
+import { theme } from 'styles/theme';
+
+import FiraSansLight from '../fonts/FiraSans-Light.ttf';
+import FiraSansRegular from '../fonts/FiraSans-Regular.ttf';
+import OswaldRegular from '../fonts/Oswald-Regular.ttf';
+import OswaldBold from '../fonts/Oswald-Bold.ttf';
+
+export const GlobalStyles = css`
+  @font-face {
+    font-family: 'Fira Sans';
+    font-weight: 300;
+    font-style: normal;
+    src: url(${FiraSansLight}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Fira Sans';
+    font-weight: 400;
+    font-style: normal;
+    src: url(${FiraSansRegular}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Oswald';
+    font-weight: 400;
+    font-style: normal;
+    src: url(${OswaldRegular}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Oswald';
+    font-weight: 700;
+    font-style: normal;
+    src: url(${OswaldBold}) format('truetype');
+  }
+
+  * {
+    margin: 0;
+  }
+
+  body {
+    background-color: ${theme.colors.white};
+    font-family: 'Fira Sans', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    letter-spacing: -0.04em;
+    text-align: justify;
+    font-style: normal;
+    color: ${theme.colors.black};
+
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: 'Oswald', sans-serif;
+    font-size: 48px;
+    line-height: 1;
+    text-transform: uppercase;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul,
+  ol {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    font: inherit;
+    color: inherit;
+    background-color: transparent;
+    cursor: pointer;
+  }
+`;
---
+++ src/styles/theme.jsx
@@ -0,0 +1,17 @@
+export const theme = Object.freeze({
+  colors: {
+    black: '#173d33',
+
+    green: '#97d28b',
+    greenLight: '#dcefd8',
+
+    grayLight: '#eaedf1',
+    gray: '#bdbdbd',
+
+    white: '#f3f5fa',
+  },
+
+  animation: {
+    transition: '0.3s ease',
+  },
+});
